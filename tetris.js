42a2f63955288bbc012ff31d8956e8a0f2ed7c49
r_kermode <r_kermode@manx.net>
2018-03-06 09:46:49 -0500

    Fixes

--- tetris.js
+++ tetris.js
@@ -2,5 +2,5 @@ const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
 context.fillStyle='#000';
-context.fillRect(0,0,canvas.height,canvas.width);
+context.fillRect(0,0,canvas.width,canvas.height);
 
